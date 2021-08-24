275157a736a28756443ef9efb6fb074021129d7c
tharlinho2 <tharlinho2@gmail.com>
2021-08-24 11:11:56 -0300

    Cronômetro

---
+++ app/src/main/java/com/tharles/cronometro/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.tharles.cronometro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import com.tharles.cronometro.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var running = false
+    var pause: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iniciar.setOnClickListener {
+            IniciarCronometro()
+        }
+
+        binding.pausar.setOnClickListener {
+            PausarCronometro()
+        }
+
+        binding.zerar.setOnClickListener {
+            ZerarCronometro()
+        }
+    }
+
+    private fun IniciarCronometro(){
+        if (!running){
+            binding.cronometro.base = SystemClock.elapsedRealtime() - pause
+            binding.cronometro.start()
+            running = true
+        }
+    }
+
+    private fun PausarCronometro(){
+        if (running){
+            binding.cronometro.stop()
+            pause = SystemClock.elapsedRealtime() - binding.cronometro.base
+            running = false
+        }
+    }
+
+    private fun ZerarCronometro(){
+        binding.cronometro.base = SystemClock.elapsedRealtime()
+        pause = 0
+    }
+}
